4cb8532574cb802c9c5945e47a53eee1d6fbd10f
kaancglyn <kaancaglayan5391@gmail.com>
2026-05-11 22:31:35 +0300

    Add files via upload

---
+++ Merhaba_c++.cpp
@@ -0,0 +1,36 @@
+
+#include <iostream>
+using namespace std;
+
+string isim1;
+int z;
+
+int x;
+int y;
+
+int main(){
+    cout << "KALAN BULMA YARIŞMASINA HOŞGELDİNİZ" << endl;
+    cout << "ADINIZI GİRİNİZ: " << endl;
+    getline(cin,isim1);
+    cout << "HOŞGELDİNİZ " << isim1 << endl;
+    cout << "İLK SAYINIZI GİRİNİZ: ";
+    cin >> x ;
+    cout << "İKİNCİ SAYINIZI GİRİNİZ: ";
+    cin >> y ;
+    
+    cout << "SONUÇ= " << endl;
+    cout << x % y;
+    
+    
+
+
+  
+    
+    
+
+
+
+    
+    
+
+}
